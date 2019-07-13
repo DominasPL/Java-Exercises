143f2a34d97f7dbeadd3a7016d3d3de6d151d7c6
DomiasPL <dominik-stepuch@wp.pl>
2019-07-13 19:38:50 +0200

    String Exercise11 second mist frequent character in given string

---
+++ src/Test/String/w3resoruceExercises/Exercise10.java
@@ -0,0 +1,47 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to check whether a given string starts with the contents of another string.
+
+        String userStr = Exercise1.getString(new Scanner(System.in));
+        String startStr = "Red";
+
+        boolean value1 = checkIfStrStartsWith(userStr, startStr);
+        System.out.println(value1);
+
+        boolean value2 = checkIfStrStartsWith1(userStr, startStr);
+        System.out.println(value2);
+
+    }
+
+    private static boolean checkIfStrStartsWith1(String userStr, String startStr) {
+
+        char[] userCharArray = userStr.toCharArray();
+        char[] startCharArray = startStr.toCharArray();
+
+        boolean value = true;
+
+        for (int i = 0; i < startCharArray.length ; i++) {
+
+            if (!(startCharArray[i] == userCharArray[i])) {
+                value = false;
+                break;
+            }
+
+        }
+
+        return value;
+    }
+
+    private static boolean checkIfStrStartsWith(String userStr, String startStr) {
+
+        return userStr.startsWith(startStr);
+    }
+
+
+}
---
+++ src/Test/String/w3resoruceExercises/Exercise11.java
@@ -0,0 +1,62 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.*;
+
+public class Exercise11 {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to find the second most frequent character in a given string.
+
+        String userStr = Exercise1.getString(new Scanner(System.in));
+
+        Map<Character, Integer> charMap = createCharMap(userStr);
+
+        findSecondMostFrequentChar(charMap);
+
+    }
+
+    private static Map<Character, Integer> createCharMap(String userStr) {
+
+        Map<Character, Integer> charsOccurances = new HashMap<>();
+
+        for (Character character : userStr.toLowerCase().toCharArray()) {
+
+            if (!charsOccurances.containsKey(character))
+                charsOccurances.put(character, 1);
+            else {
+                Integer value = charsOccurances.get(character);
+                charsOccurances.put(character, value + 1);
+            }
+        }
+
+        return charsOccurances;
+    }
+
+    private static void findSecondMostFrequentChar(Map<Character, Integer> charMap) {
+
+        Set<Integer> occurances = new TreeSet<>();
+
+        for (Character character : charMap.keySet()) {
+            occurances.add(charMap.get(character));
+        }
+
+        List<Integer> occurancesList = new ArrayList<>(occurances);
+
+        Integer secondMostFrequentValue = occurancesList.get(occurances.size() - 2);
+
+        List<Character> secondMostFrequentChars = new LinkedList<>();
+
+        for (Character character : charMap.keySet()) {
+
+            if (charMap.get(character).equals(secondMostFrequentValue))
+                secondMostFrequentChars.add(character);
+        }
+
+        System.out.println("Second most frequent chars in your string are: " + secondMostFrequentChars);
+
+    }
+
+
+
+}
