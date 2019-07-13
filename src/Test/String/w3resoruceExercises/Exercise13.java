1634ef5589274b91d2f2f6a57aa781a499ea3088
DomiasPL <dominik-stepuch@wp.pl>
2019-07-13 20:48:49 +0200

    String Exercise13  Java program to print list items containing all characters of a given word

---
+++ src/Test/String/w3resoruceExercises/Exercise13.java
@@ -0,0 +1,84 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise13 {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to print list items containing all characters of a given word.
+        List<String> items = getItemsList(new Scanner(System.in));
+        String chars = "ar";
+
+        getItemsContainingCharacters(items, chars);
+
+    }
+
+    private static List<String> getItemsList(Scanner scanner) {
+
+        List<String> items = new LinkedList<>();
+        String item;
+        String choice;
+
+        while (true) {
+
+            System.out.print("Do you want to add an item (Y/N): ");
+            choice = scanner.next();
+            scanner.nextLine();
+
+            if (choice.equals("y") || choice.equals("Y")) {
+
+                System.out.print("Give an item name: ");
+
+                if (scanner.hasNextLine()) {
+                    item = scanner.nextLine();
+                    items.add(item);
+                } else {
+                    System.out.println("Incorrect value. Try again!");
+                }
+
+
+            } else if (choice.equals("n") || choice.equals("N"))
+                return items;
+
+            else {
+                System.out.println("Incorrect value. Try again!");
+            }
+
+        }
+
+
+    }
+
+    private static void getItemsContainingCharacters(List<String> items, String chars) {
+
+        char[] charArray = chars.toCharArray();
+        List<String> newItemList = new LinkedList<>();
+
+        for (String str : items) {
+
+            boolean appearInStr = true;
+            for (int i = 0; i < charArray.length; i++) {
+
+                if (str.indexOf(charArray[i]) < 0) {
+                    appearInStr = false;
+                }
+
+            }
+
+            if (appearInStr)
+                newItemList.add(str);
+
+        }
+
+        newItemList.forEach(System.out::println);
+
+    }
+
+
+
+
+
+}
