7ccfd9de8081a7eb8a77d6d5fd41bc2e8f334762
DomiasPL <dominik-stepuch@wp.pl>
2019-07-13 17:43:10 +0200

    String Exercise8 letters replace

---
+++ src/Test/String/w3resoruceExercises/Exercise8.java
@@ -0,0 +1,43 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise8 {
+
+    public static void main(String[] args) {
+
+        //Write a Java program to replace all the 'd' characters with 'f' characters.
+
+        String userStr = Exercise1.getString(new Scanner(System.in));
+
+//        String replacedCharStr = replaceLetters(userStr);
+//        System.out.println(replacedCharStr);
+
+        String replacedCharStr = replaceLetters1(userStr);
+        System.out.println(replacedCharStr);
+    }
+
+    private static String replaceLetters1(String userStr) {
+
+        char[] charArray = userStr.toLowerCase().toCharArray();
+
+        for (int i = 0; i < charArray.length ; i++) {
+
+            if (charArray[i] == 'd')
+                charArray[i] = 'f';
+
+        }
+
+        return String.valueOf(charArray);
+
+    }
+
+    private static String replaceLetters(String userStr) {
+
+
+        return userStr.toLowerCase().replaceAll("d", "f");
+    }
+
+
+}
