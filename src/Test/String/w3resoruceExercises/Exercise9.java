dcbc3ad44357bb720b3851de633c9f902f87f0ca
DomiasPL <dominik-stepuch@wp.pl>
2019-07-13 17:55:26 +0200

    String Exercies9 str replacement

---
+++ src/Test/String/w3resoruceExercises/Exercise9.java
@@ -0,0 +1,25 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.Scanner;
+
+public class Exercise9 {
+
+    public static void main(String[] args) {
+
+        String userStr = Exercise1.getString(new Scanner(System.in));
+        String toReplace = "cat";
+        String replacement = "dog";
+
+        String newStr = replaceLetters(userStr, toReplace, replacement);
+        System.out.println(newStr);
+
+    }
+
+    private static String replaceLetters(String userStr, String toReplace, String replacement) {
+
+        return userStr.toLowerCase().replaceAll(toReplace, replacement);
+
+    }
+
+
+}
