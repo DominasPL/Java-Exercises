973a4c9abd1a42614afa0c5a8b7ecc61ec1b71c5
DomiasPL <dominik-stepuch@wp.pl>
2019-07-14 13:24:38 +0200

    String Exercise13 remove b and ac from given string

---
+++ src/Test/String/w3resoruceExercises/Exercise14.java
@@ -0,0 +1,32 @@
+package Test.String.w3resoruceExercises;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Exercise14 {
+
+
+    public static void main(String[] args) {
+
+        //Write a Java program to remove "b" and "ac" from a given string.
+
+        String userStr = Exercise1.getString(new Scanner(System.in));
+
+        String newStr = removeCharacters(userStr);
+        System.out.println(newStr);
+
+
+
+    }
+
+    private static String removeCharacters(String userStr) {
+
+        return userStr.replace("ac", "").replace("b", "");
+    }
+    
+
+}
