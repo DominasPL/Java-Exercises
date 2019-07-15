db2a850421899eba64242f4a2be996d17eae2b99
DomiasPL <dominik-stepuch@wp.pl>
2019-07-15 20:12:34 +0200

    Shuffle and reverse elements in list solutions

--- src/Test/String/w3resoruceExercises/Collections/List/Exercise5.java
+++ src/Test/String/w3resoruceExercises/Collections/List/Exercise5.java
@@ -14,13 +14,42 @@ public class Exercise5 {
 
         List<String> strList = Exercise1.addStrElementsToArrayList(new ArrayList<>());
 
+        //Shuffle elements first method
         List<String> shuffledListElements = shuffleListElements(strList);
         System.out.println(shuffledListElements);
+        //Reverse elements first method
+        List<String> reversedElements = reverseElements(shuffledListElements);
+        System.out.println(reversedElements);
 
+//      Shuffle elements second method
         List<String> shuffledListElements1 = shuffleListElements1(strList);
         System.out.println(shuffledListElements1);
+        //Reverse elements second method
+        List<String> reversedElements1 = reversedElements1(shuffledListElements1);
+        System.out.println(reversedElements1);
 
     }
+
+    private static List<String> reverseElements(List<String> strList) {
+
+        Collections.reverse(strList);
+
+        return strList;
+
+    }
+
+
+    private static List<String> reversedElements1(List<String> shuffledListElements1) {
+
+        List<String> reversedList = new ArrayList<>();
+
+        for (int i = shuffledListElements1.size() - 1; i >= 0 ; i--) {
+            reversedList.add(shuffledListElements1.get(i));
+        }
+
+        return reversedList;
+    }
+
 
     private static List<String> shuffleListElements(List<String> strList) {
 
