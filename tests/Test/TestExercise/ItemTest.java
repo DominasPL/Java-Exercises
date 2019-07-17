5c3be91bfe51d5e9217eaf14ca8158056e5a0ac5
DomiasPL <dominik-stepuch@wp.pl>
2019-07-17 17:20:21 +0200

    Added new test to check item is not null

--- tests/Test/TestExercise/ItemTest.java
+++ tests/Test/TestExercise/ItemTest.java
@@ -6,6 +6,14 @@ import static org.junit.Assert.*;
 
 public class ItemTest {
 
+    @Test
+    public void itemShouldNotBeNull() {
+
+        Item item = new Item("Apple", 12);
+        assertNotNull(item);
+
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowIllegalArgumentExceptionOnWrongPriceParameter() {
         new Item("Apple", -10);
